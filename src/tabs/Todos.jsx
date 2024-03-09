995a8c904fe25574dbc3f8ebfc0308e277cfb040
karolina taran <karolina.taran@gmail.com>
2024-03-09 14:04:49 +0100

    addTask1

--- src/tabs/Todos.jsx
+++ src/tabs/Todos.jsx
@@ -1,5 +1,21 @@
-import { Text } from 'components';
+import { Form, Text, TodoList } from 'components';
+import { nanoid } from 'nanoid';
+import { useState } from 'react';
 
 export const Todos = () => {
-  return <Text textAlign="center">There are no any todos ...</Text>;
+  const [todos, setTodos] = useState([]);
+  const handleSubmit = text => {
+    const newTodo = {
+      id: nanoid(),
+      text,
+    };
+    setTodos(prev => [...prev, newTodo]);
+  };
+  return (
+    <>
+      <Form onSubmit={handleSubmit} />
+      <Text textAlign="center">There are no any todos ...</Text>;
+      <TodoList />
+    </>
+  );
 };
