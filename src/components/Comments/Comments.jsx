a4cfd7d495f9c85f730ad1afa95eace6a1705b4d
Victoriia Oleh <slavika86@gmail.com>
2024-04-20 17:10:24 +0300

    add filter function

--- src/components/Comments/Comments.jsx
+++ src/components/Comments/Comments.jsx
@@ -4,17 +4,27 @@ import { Comment } from "../Comment/Comment";
 import { Grid } from "../Grid/Grid";
 import { useGetAllCommentsQuery } from "../../redux/commentApi";
 import { Loader } from "../Loader/Loader";
+import { useSelector } from "react-redux";
+import { selectFilter } from "../../redux/filterSlice";
 // import { comments } from '../../helpers/comments';
 
 export const Comments = () => {
   const { data: comments, isLoading } = useGetAllCommentsQuery();
-
+  const filter = useSelector(selectFilter);
+  const getFilteredComments = () => {
+    return comments.filter((comment) => {
+      console.log(comment);
+      return comment.content.toLowerCase().includes(filter.toLowerCase());
+    });
+  };
   return (
     <>
       {isLoading && <Loader />}
       <Grid>
         {comments &&
-          comments.map((comment) => <Comment key={comment.id} {...comment} />)}
+          getFilteredComments().map((comment) => (
+            <Comment key={comment.id} {...comment} />
+          ))}
       </Grid>
     </>
   );
--- src/components/Filter/Filter.jsx
+++ src/components/Filter/Filter.jsx
@@ -1,8 +1,14 @@
-import React from 'react';
-import { CiSearch } from 'react-icons/ci';
-import styles from './Filter.module.css';
+import React from "react";
+import { CiSearch } from "react-icons/ci";
+import styles from "./Filter.module.css";
+import { useDispatch } from "react-redux";
+import { setFilter } from "../../redux/filterSlice";
 
 export const Filter = () => {
+  const dispatch = useDispatch();
+  const handleChange = (event) => {
+    dispatch(setFilter(event.target.value));
+  };
   return (
     <div className={styles.search}>
       <div className={styles.searchWrapper}>
@@ -10,9 +16,10 @@ export const Filter = () => {
 
         <input
           className={styles.searchInput}
-          type='text'
-          id='search'
-          placeholder='Search something..'
+          type="text"
+          id="search"
+          placeholder="Search something.."
+          onChange={handleChange}
         />
       </div>
     </div>
